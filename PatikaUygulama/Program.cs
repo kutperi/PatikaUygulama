1b31832c4abf36bb99be2043ed97718e744bccf1
Ebubekir <ebbkrgnn@gmail.com>
2024-07-16 21:30:16 +0300

    Add project files.

---
+++ PatikaUygulama/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PatikaBootcamp
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bilal Bilin");
+            Console.WriteLine("Patika kursuna başladığım için çok heyecanlıyım, yazılım dünyasına somut bir adım atmak için buradayım, bu programı verilen sürede öğreneceğime inanıyorum bu konuda Patika ekibine güveniyorum.");
+
+        }
+    }
+}
